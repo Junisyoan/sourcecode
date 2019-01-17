a3f377c93cd01690389e75e30103fbb2920fe9ed
Junisyoan <45847365+Junisyoan@users.noreply.github.com>
2019-01-17 17:39:09 +0800

    Update UserBizImpl.java

--- src/xyz/cymedical/biz/impl/jun/UserBizImpl.java
+++ src/xyz/cymedical/biz/impl/jun/UserBizImpl.java
@@ -4,66 +4,31 @@ import java.util.List;
 
 import javax.annotation.Resource;
 
-import org.apache.log4j.Logger;
+
 import org.springframework.stereotype.Service;
 
-import xyz.cymedical.biz.inter.jun.UserBiz;
+import xyz.cymedical.biz.jun.UserBiz;
 import xyz.cymedical.entity.jun.User;
 import xyz.cymedical.mapper.jun.UserMapper;
 
 @Service("userBiz")
-public class UserBizImpl extends BaseImpl implements UserBiz{
-
+public class UserBizImpl implements UserBiz
+{
+	
+//	private SqlSession conn = MyBatisTool.getSession();
+//	private UserMapper userMapper = conn.getMapper(UserMapper.class);
+	
 	@Resource
 	private UserMapper userMapper;
 	
-	public UserBizImpl() {
-		logger=Logger.getLogger(UserBizImpl.class);
-	}
-
 	@Override
-	public User queryByLogin(User user) {
-		return userMapper.queryByLogin(user);
-	}
-
-	@Override
-	public boolean queryAccount(String account) {
-		if (userMapper.queryAccount(account)!=null) {
-			isExist=true;
-		} else {
-			isExist=false;
-		}
-		return isExist;
-	}
-
-	@Override
-	public boolean addUser(User user) {
-		return userMapper.addUser(user);
-	}
-
-	@Override
-	public List<User> queryUserList(int pageNum) {
-		firstRecord = (pageNum-1)*10;
-		lastRecord = pageNum*10+1;
-		return userMapper.queryUserList(firstRecord,lastRecord);
-	}
-
-	@Override
-	public int queryUserCount() {
+	public List<User> findAll()
+	{
+		// TODO Auto-generated method stub
 		
-		System.out.println("userMapper"+userMapper);
-		return userMapper.queryUserCount();
+		return userMapper.findAll();
 	}
 
-	@Override
-	public boolean updateUser(User user) {
-		return userMapper.updateUser(user);
-	}
 
-	@Override
-	public boolean delUser(int user_id) {
-		return userMapper.delUser(user_id);
-	}
-	
 	
 }
