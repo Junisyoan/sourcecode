46db7ac8a353ac8cd3d559c5c12c344fdac062e6
1720787484 <46743935+1720787484@users.noreply.github.com>
2019-01-18 09:28:31 +0800

    Create TbUserBizImp.java

---
+++ src/xyz/cymedical/biz/imp/jiang/TbUserBizImp.java
@@ -0,0 +1,24 @@
+package xyz.cymedical.biz.imp.jiang;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import xyz.cymedical.bean.jiang.Tb_user;
+import xyz.cymedical.biz.jiang.TbUserBiz;
+import xyz.cymedical.mapper.jiang.TbUserMapper;
+@Service("tbUserBiz")
+public class TbUserBizImp implements TbUserBiz{
+
+	
+	@Resource
+	TbUserMapper tbUserMapper;
+	@Override
+	public List<Tb_user> findUser(Tb_user user) {
+		// TODO Auto-generated method stub
+		return (List<Tb_user>) tbUserMapper.findUser(user);
+	}
+
+}
