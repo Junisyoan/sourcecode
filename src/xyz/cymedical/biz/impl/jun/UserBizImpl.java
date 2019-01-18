150a1b31ebf91adf8da8ff4278085e8df8a544a6
437075478 <46699711+437075478@users.noreply.github.com>
2019-01-18 09:50:51 +0800

    Update UserBizImpl.java

--- src/xyz/cymedical/biz/impl/jun/UserBizImpl.java
+++ src/xyz/cymedical/biz/impl/jun/UserBizImpl.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import javax.annotation.Resource;
 
-
 import org.springframework.stereotype.Service;
 
 import xyz.cymedical.biz.jun.UserBiz;
@@ -12,23 +11,19 @@ import xyz.cymedical.entity.jun.User;
 import xyz.cymedical.mapper.jun.UserMapper;
 
 @Service("userBiz")
-public class UserBizImpl implements UserBiz
-{
-	
+public class UserBizImpl implements UserBiz {
+
 //	private SqlSession conn = MyBatisTool.getSession();
 //	private UserMapper userMapper = conn.getMapper(UserMapper.class);
-	
+
 	@Resource
 	private UserMapper userMapper;
-	
+
 	@Override
-	public List<User> findAll()
-	{
+	public List<User> findAll() {
 		// TODO Auto-generated method stub
-		
+
 		return userMapper.findAll();
 	}
 
-
-	
 }
