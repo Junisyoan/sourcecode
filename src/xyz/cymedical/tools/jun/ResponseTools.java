4a5915d19b0b2a61858b6cf56f1bdfb25b0358a3
Junisyoan <45847365+Junisyoan@users.noreply.github.com>
2019-01-19 17:21:24 +0800

    Update ResponseTools.java

--- src/xyz/cymedical/tools/jun/ResponseTools.java
+++ src/xyz/cymedical/tools/jun/ResponseTools.java
@@ -7,16 +7,12 @@ package xyz.cymedical.tools.jun;
 */
 public class ResponseTools {
 
-	private static String strMsg;
-	
-	public ResponseTools() {
-		strMsg = "<script type='text/javascript'>alert('###');window.history.go(-1);</script>";
-	}
+	private static String strMsgBack="<script type='text/javascript'>alert('###');window.history.go(-1);</script>";
 	
 	/*
 	 * 返回数据并且 跳回 跳转之前的页面
 	 */
 	public static String returnMsgAndBack(String msg) {
-		return strMsg.replace("###", msg);
+		return strMsgBack.replace("###", msg);
 	}
 }
