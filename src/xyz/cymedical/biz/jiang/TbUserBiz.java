a7ef504b37a4889e031ab138ab9f3ffa576b80c4
Junisyoan <45847365+Junisyoan@users.noreply.github.com>
2019-01-19 08:22:46 +0800

    Update TbUserBiz.java

--- src/xyz/cymedical/biz/jiang/TbUserBiz.java
+++ src/xyz/cymedical/biz/jiang/TbUserBiz.java
@@ -2,11 +2,13 @@ package xyz.cymedical.biz.jiang;
 
 import java.util.List;
 
-import xyz.cymedical.bean.jiang.Tb_user;
+import xyz.cymedical.entity.jiang.Tb_user;
  
   
 public interface TbUserBiz {
 	
 	public List<Tb_user>  findUser(Tb_user user);
+	
+	public List<Tb_user> findAll();
 
 }
