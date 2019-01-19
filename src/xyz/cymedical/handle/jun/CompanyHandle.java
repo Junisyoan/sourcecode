5f26ba1165f93686fbadc413aed2dbe48f270c62
Junisyoan <45847365+Junisyoan@users.noreply.github.com>
2019-01-19 15:31:57 +0800

    Update CompanyHandle.java

--- src/xyz/cymedical/handle/jun/CompanyHandle.java
+++ src/xyz/cymedical/handle/jun/CompanyHandle.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,7 +19,7 @@ import xyz.cymedical.tools.jun.ResponseTools;
 * @author Junisyoan
 * @version 1.0
 */
-
+@Controller
 @RequestMapping("/company")
 public class CompanyHandle {
 
