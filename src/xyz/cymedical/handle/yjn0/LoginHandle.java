174da4676fb0305047602d8848015d3e37c49d15
Junisyoan <45847365+Junisyoan@users.noreply.github.com>
2019-01-19 14:36:30 +0800

    Delete LoginHandle.java

--- src/xyz/cymedical/handle/yjn0/LoginHandle.java
+++
@@ -1,9 +0,0 @@
-package xyz.cymedical.handle.yjn0;
-//package xyz.cyedical.handle.yjn;
-//
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//@RequestMapping("/user")
-//public class LoginHandle {
-//
-//}
