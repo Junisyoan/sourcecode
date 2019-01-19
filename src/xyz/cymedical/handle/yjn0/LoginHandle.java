a46de5b64f8736941125aa08ba09c81d6e94f17b
Junisyoan <45847365+Junisyoan@users.noreply.github.com>
2019-01-19 08:23:56 +0800

    Create LoginHandle.java

---
+++ src/xyz/cymedical/handle/yjn0/LoginHandle.java
@@ -0,0 +1,9 @@
+package xyz.cymedical.handle.yjn0;
+//package xyz.cyedical.handle.yjn;
+//
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@RequestMapping("/user")
+//public class LoginHandle {
+//
+//}
