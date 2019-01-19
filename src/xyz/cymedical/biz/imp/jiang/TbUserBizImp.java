a0c0605c367319fbcc3e5a8b686c7bcc0d5196b4
Junisyoan <45847365+Junisyoan@users.noreply.github.com>
2019-01-19 08:22:42 +0800

    Update TbUserBizImp.java

--- src/xyz/cymedical/biz/imp/jiang/TbUserBizImp.java
+++ src/xyz/cymedical/biz/imp/jiang/TbUserBizImp.java
@@ -6,18 +6,24 @@ import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
 
-import xyz.cymedical.bean.jiang.Tb_user;
 import xyz.cymedical.biz.jiang.TbUserBiz;
+import xyz.cymedical.entity.jiang.Tb_user;
 import xyz.cymedical.mapper.jiang.TbUserMapper;
-@Service("tbUserBiz")
+
+@Service("userBiz")
 public class TbUserBizImp implements TbUserBiz{
 
-	
 	@Resource
 	TbUserMapper tbUserMapper;
+	
 	@Override
 	public List<Tb_user> findUser(Tb_user user) {
 		return (List<Tb_user>) tbUserMapper.findUser(user);
+	}
+	@Override
+	public List<Tb_user> findAll() {
+		// TODO Auto-generated method stub
+		return tbUserMapper.findAll();
 	}
 
 }
