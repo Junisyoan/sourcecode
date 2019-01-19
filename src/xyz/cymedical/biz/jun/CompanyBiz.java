e4c79ff64be9144309621da5fb1f040f37b190f0
Junisyoan <45847365+Junisyoan@users.noreply.github.com>
2019-01-19 23:05:34 +0800

    Update CompanyBiz.java

--- src/xyz/cymedical/biz/jun/CompanyBiz.java
+++ src/xyz/cymedical/biz/jun/CompanyBiz.java
@@ -16,4 +16,25 @@ public interface CompanyBiz {
 	 * @return	返回已被注册、注册成功、注册失败
 	 */
 	public String regCompany(Company company);
+	
+	/**
+	 * 查询公司名字
+	 * @param name	公司名字
+	 * @return	是否存在
+	 */
+	public boolean queryName(String name);
+	
+	/**
+	 * 查询账户是否存在
+	 * @param account	账户
+	 * @return	是否存在
+	 */
+	public boolean queryAccount(String account);
+	
+	/**
+	 * 查询公司座机号
+	 * @param tel	座机号
+	 * @return	是否存在
+	 */
+	public boolean queryTel(String tel);
 }
