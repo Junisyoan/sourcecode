bca0ef7ea7e7f0e3191406b94a5edfccd16b3b2e
Junisyoan <45847365+Junisyoan@users.noreply.github.com>
2019-01-19 14:36:48 +0800

    Create ResponseTools.java

---
+++ src/xyz/cymedical/tools/jun/ResponseTools.java
@@ -0,0 +1,22 @@
+package xyz.cymedical.tools.jun;
+
+/**
+* 2019年1月19日
+* @author Junisyoan
+* @version 1.0
+*/
+public class ResponseTools {
+
+	private static String strMsg;
+	
+	public ResponseTools() {
+		strMsg = "<script type='text/javascript'>alert('###');window.history.go(-1);</script>";
+	}
+	
+	/*
+	 * 返回数据并且 跳回 跳转之前的页面
+	 */
+	public static String returnMsgAndBack(String msg) {
+		return strMsg.replace("###", msg);
+	}
+}
