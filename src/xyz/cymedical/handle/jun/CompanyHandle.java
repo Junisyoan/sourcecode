bdcba87b284aee11ba8b7c6a3ac54d260923b017
Junisyoan <45847365+Junisyoan@users.noreply.github.com>
2019-01-20 19:57:22 +0800

    Update CompanyHandle.java

--- src/xyz/cymedical/handle/jun/CompanyHandle.java
+++ src/xyz/cymedical/handle/jun/CompanyHandle.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import xyz.cymedical.biz.jun.CompanyBiz;
 import xyz.cymedical.entity.jun.Company;
@@ -27,6 +28,29 @@ public class CompanyHandle {
 	
 	public CompanyHandle() {
 	}
+	
+	/*
+	 * 公司用户登录
+	 */
+	@RequestMapping(value="/loginCompany.handle", method=RequestMethod.POST)
+	public ModelAndView	companyLogin(HttpServletResponse response, String userName,String password) {
+		System.out.println("用户登录"+userName);
+		Company company = companyBiz.companyLogin(userName, password);
+		ModelAndView modelAndView = new ModelAndView();
+		if (company!=null) {
+			modelAndView.setViewName("WEB-INF/medical_workstation/company-manage");
+			return modelAndView;
+		}else {
+			try {
+				response.getWriter().println(ResponseTools.returnMsgAndBack("用户或者密码错误"));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return null;
+		}
+		
+	}
+	
 	
 	/*
 	 * 公司注册
@@ -51,7 +75,7 @@ public class CompanyHandle {
 				
 			case "注册成功":
 				System.out.println("公司注册成功");
-				response.getWriter().println(ResponseTools.returnMsgAndBack(res));
+				response.getWriter().println(ResponseTools.returnMsgAndRedirect(res, ""));
 				break;
 				
 			default:
