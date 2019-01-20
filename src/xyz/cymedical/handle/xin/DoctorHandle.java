5eee0fcf691acc130cca74f2132501e2979a6396
524074949 <46743055+524074949@users.noreply.github.com>
2019-01-20 23:42:42 +0800

    Update DoctorHandle.java

--- src/xyz/cymedical/handle/xin/DoctorHandle.java
+++ src/xyz/cymedical/handle/xin/DoctorHandle.java
@@ -4,6 +4,7 @@ import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import xyz.cymedical.biz.xin.DoctorBiz;
 
@@ -13,5 +14,18 @@ import xyz.cymedical.biz.xin.DoctorBiz;
 public class DoctorHandle {
 
 	@Resource
-	private DoctorBiz doctor; 			//公司的业务逻辑
+	private DoctorBiz doctorbiz; 			//医生的业务逻辑
+	
+	@RequestMapping(value = "/findAll.handle")
+	public ModelAndView findAll() {
+
+		System.out.println("2222");
+
+		System.out.println(doctorbiz.findMyDetails(""));
+
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("WEB-INF/view.jiang/index");
+		return mav;
+
+	}
 }
