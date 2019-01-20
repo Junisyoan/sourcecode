8cbd0b71e9bd361d9333b6fb8088b1fe9f18bf7f
437075478 <46699711+437075478@users.noreply.github.com>
2019-01-20 20:37:46 +0800

    Create LogCompanyHandle.java

---
+++ src/xyz/cymedical/handle/ctx/LogCompanyHandle.java
@@ -0,0 +1,43 @@
+package xyz.cymedical.handle.ctx;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import xyz.cymedical.biz.ctx.LogCompanyBiz;
+import xyz.cymedical.entity.ctx.LogCompany;
+
+@Controller
+@RequestMapping("/logcompany")
+public class LogCompanyHandle {
+
+	@Resource
+	public LogCompanyBiz logCompanyBiz;
+
+	private List<LogCompany> logCompanylist = new ArrayList<LogCompany>();
+
+	public LogCompanyHandle() {
+
+	}
+
+	// 公司记账
+	@RequestMapping(value = "/findcompanylog.action")
+	public ModelAndView findcompanylog() {
+
+		// 没写完，数据未获取
+		String name = "";
+
+		logCompanylist = logCompanyBiz.queryByName(name);
+
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("/WEB-INF/companylog");
+		mav.addObject("logCompanylist", logCompanylist);
+		return mav;
+
+	}
+}
