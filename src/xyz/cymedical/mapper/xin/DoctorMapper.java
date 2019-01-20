f97f760405b02cafdf1b278162be709c642df04d
524074949 <46743055+524074949@users.noreply.github.com>
2019-01-20 23:42:30 +0800

    Create DoctorMapper.java

---
+++ src/xyz/cymedical/mapper/xin/DoctorMapper.java
@@ -0,0 +1,28 @@
+package xyz.cymedical.mapper.xin;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import xyz.cymedical.entity.jun.Company;
+import xyz.cymedical.entity.xin.Detail;
+
+/**
+* 2019年1月19日
+* @author Junisyoan
+* @version 1.0
+*/
+
+@Repository
+public interface DoctorMapper {
+
+	//query
+	
+	/**
+	 * 公司信息
+	 * @param code	条码号
+	 * @return	细项列表
+	 */
+	public List<Detail> findMyDetails(String code);
+}
