49167440f6b1702959dc099c2e1e8be2bffa7e47
437075478 <46699711+437075478@users.noreply.github.com>
2019-01-21 22:32:35 +0800

    Update PatientMapper.java

--- src/xyz/cymedical/mapper/ctx/PatientMapper.java
+++ src/xyz/cymedical/mapper/ctx/PatientMapper.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
+import xyz.cymedical.entity.jun.Patient;
+
 @Repository
 public interface PatientMapper {
 
 	public List<Patient> query(@Param("name") String name, @Param("phone") String phone, @Param("time") String time,
-			@Param("check_num") String check_num);
+			@Param("code") String code);
 
 }
