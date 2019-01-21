39d23328494abd4840e7e4b9e8f458cde9a0f112
524074949 <46743055+524074949@users.noreply.github.com>
2019-01-21 16:00:21 +0800

    Update DoctorHandle.java

--- src/xyz/cymedical/handle/xin/DoctorHandle.java
+++ src/xyz/cymedical/handle/xin/DoctorHandle.java
@@ -1,5 +1,7 @@
 package xyz.cymedical.handle.xin;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import xyz.cymedical.biz.xin.DoctorBiz;
+import xyz.cymedical.entity.zsc.Project;
 
 
 @Controller
@@ -16,15 +19,19 @@ public class DoctorHandle {
 	@Resource
 	private DoctorBiz doctorbiz; 			//医生的业务逻辑
 	
-	@RequestMapping(value = "/findAll.handle")
-	public ModelAndView findAll() {
+	@RequestMapping(value = "/findProject.handle")
+	public ModelAndView findProject(String onecode) {
 
-		System.out.println("2222");
+		System.out.println("onecode="+onecode);
 
-		System.out.println(doctorbiz.findMyDetails(""));
+		System.out.println(doctorbiz.findMyProject(onecode));
+		
+//		List<Project> plist = 
+		
+		
 
 		ModelAndView mav = new ModelAndView();
-		mav.setViewName("WEB-INF/view.jiang/index");
+		mav.setViewName("WEB-INF/doctor.xin/pro_receive");
 		return mav;
 
 	}
@@ -34,7 +41,7 @@ public class DoctorHandle {
 
 		System.out.println("index....");
 
-		System.out.println(doctorbiz.findMyDetails(""));
+		System.out.println(doctorbiz.findMyProject(""));
 
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("WEB-INF/doctor.xin/pro_receive");
