bce47b03b55fe8934dd006c44896332d1be61677
451942369 <451942369@qq.com>
2019-01-22 09:12:31 +0800

    Update LoginHandle.java

--- src/xyz/cymedical/handle/yjn/LoginHandle.java
+++ src/xyz/cymedical/handle/yjn/LoginHandle.java
@@ -12,10 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import xyz.cymedical.biz.ctx.LogCompanyBiz;
 import xyz.cymedical.biz.jiang.TbUserBiz;
-import xyz.cymedical.biz.xin.DoctorBiz;
-import xyz.cymedical.entity.ctx.LogCompany;
 import xyz.cymedical.entity.jiang.Tb_user;
 
 @Controller // 此注释的含义是将该类设置成为浏览器提交的上来的类
@@ -24,14 +21,6 @@ public class LoginHandle {
 
 	@Resource
 	private TbUserBiz userBiz;
-
-	@Resource
-	private DoctorBiz doctorbiz; // 医生的业务逻辑
-
-	private List<LogCompany> logCompanylist = new ArrayList<LogCompany>();
-
-	@Resource
-	public LogCompanyBiz logCompanyBiz;
 
 	@RequestMapping(value = "/login.handle", method = RequestMethod.POST, produces = "application/json;charset=utf-8")
 	public @ResponseBody String login(HttpServletRequest req, String checkCode, Tb_user user) {
@@ -46,45 +35,13 @@ public class LoginHandle {
 
 		if (user.getAccount().equals("admin") && user.getPwd().equals("123")) {
 			result = "管理员";
-		}else if(user.getAccount().equals("admin")&&user.getPwd().equals("1234")) {
+		} else if (user.getAccount().equals("admin") && user.getPwd().equals("1234")) {
 			result = "医生";
-		}else {
+		} else {
 			result = "账号密码错误";
 		}
 		return result;
 
 	}
 
-	@RequestMapping(value = "/index.handle")
-	public ModelAndView find() {
-
-		System.out.println("2222");
-
-		System.out.println(doctorbiz.findMyDetails(""));
-
-		System.out.println(userBiz.findAll());
-
-		ModelAndView mav = new ModelAndView();
-		mav.setViewName("WEB-INF/view.jiang/index");
-		return mav;
-
-	}
-
-	// 公司记账
-	@RequestMapping(value = "/findcompanylog.handle")
-	public ModelAndView findcompanylog() {
-
-		// 没写完，数据未获取
-		String name = "蓝色科技";
-
-		logCompanylist = logCompanyBiz.queryByName(name);
-
-		System.out.println(logCompanylist.size());
-
-		ModelAndView mav = new ModelAndView();
-		mav.setViewName("WEB-INF/logcompany");
-		mav.addObject("logCompanylist", logCompanylist);
-		return mav;
-
-	}
 }
