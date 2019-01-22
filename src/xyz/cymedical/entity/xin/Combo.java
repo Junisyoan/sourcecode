5b2d837ed78aefbf40ef3d5562467303f63757b1
451942369 <451942369@qq.com>
2019-01-22 14:44:06 +0800

    Update Combo.java

--- src/xyz/cymedical/entity/xin/Combo.java
+++ src/xyz/cymedical/entity/xin/Combo.java
@@ -1,5 +1,26 @@
 package xyz.cymedical.entity.xin;
 
-public class Combo {
+import java.util.List;
 
+import xyz.cymedical.entity.zsc.Project;
+
+public class Combo {
+	
+	private int combo_id;
+	private String name;
+	private float price;
+	private List<Project> projects;
+	
+	public Combo() {
+		super();
+	}
+
+	public Combo(int combo_id, String name, float price) {
+		super();
+		this.combo_id = combo_id;
+		this.name = name;
+		this.price = price;
+	}
+	
+	
 }
