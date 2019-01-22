ed254ce831f1a5b708528aa4c7e8c3f4ff3be2eb
Junisyoan <45847365+Junisyoan@users.noreply.github.com>
2019-01-22 21:45:26 +0800

    Create FileTools.java

---
+++ src/xyz/cymedical/tools/jun/FileTools.java
@@ -0,0 +1,17 @@
+package xyz.cymedical.tools.jun;
+
+
+/**
+*	@author Junisyoan;
+*	日期：2019年1月22日
+*	时间：下午9:38:21
+*	类说明：解析Excel文件
+*/
+public class FileTools {
+
+	public static int getExcelFilePeopleAmount(String filePath) {
+		
+		
+		return 0;
+	}
+}
