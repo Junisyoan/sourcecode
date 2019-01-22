6ff9210f4edd3bd923d2b0086a1e7e278215bf47
451942369 <451942369@qq.com>
2019-01-22 09:55:09 +0800

    Delete UserBiz.java

--- src/xyz/cymedical/biz/yjn/UserBiz.java
+++
@@ -1,9 +0,0 @@
-package xyz.cymedical.biz.yjn;
-
-import java.util.List;
-
-import xyz.cymedical.entity.jiang.Tb_user;
-
-public interface UserBiz {
-	public List<Tb_user> userLogin(Tb_user user);
-}
