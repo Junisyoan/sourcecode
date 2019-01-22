b282db5dd1bcbd54ab20de24f8e09b229dafc420
451942369 <451942369@qq.com>
2019-01-22 09:12:05 +0800

    Update LogBizImpl.java

--- src/xyz/cymedical/biz/imp/yjn/LogBizImpl.java
+++ src/xyz/cymedical/biz/imp/yjn/LogBizImpl.java
@@ -9,16 +9,23 @@ import org.springframework.stereotype.Service;
 import xyz.cymedical.biz.yjn.LogBiz;
 import xyz.cymedical.entity.yjn.Log;
 import xyz.cymedical.mapper.yjn.LogMapper;
+
 @Service("logBiz")
 public class LogBizImpl implements LogBiz {
 
 	@Resource
 	private LogMapper logmapper;
-	
+
 	@Override
 	public List<Log> findAllLog() {
 		// TODO Auto-generated method stub
 		return logmapper.findAllLog();
+	}
+
+	@Override
+	public boolean delLog(String log_id) {
+		// TODO Auto-generated method stub
+		return logmapper.delLog(log_id);
 	}
 
 }
