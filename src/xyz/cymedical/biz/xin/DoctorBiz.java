ab09aa15b1eeec1a25a0d4ba832b9a1b99953320
451942369 <451942369@qq.com>
2019-01-22 14:44:17 +0800

    Update DoctorBiz.java

--- src/xyz/cymedical/biz/xin/DoctorBiz.java
+++ src/xyz/cymedical/biz/xin/DoctorBiz.java
@@ -1,6 +1,7 @@
 package xyz.cymedical.biz.xin;
 
 import java.util.List;
+import java.util.Map;
 
 import xyz.cymedical.entity.jun.Company;
 import xyz.cymedical.entity.zsc.Detail;
@@ -18,6 +19,6 @@ public interface DoctorBiz {
 	 * @param code	条码号
 	 * @return	返回条码对应的病人所有已选项目
 	 */
-	public List<Detail> findMyProject(String code);
+	public List<Map<String,Object>> findMyProject(String code);
 
 }
