f90d50823589e559fbb3aafd1e55b78794adcaed
Junisyoan <45847365+Junisyoan@users.noreply.github.com>
2019-01-22 21:45:35 +0800

    Update ResponseTools.java

--- src/xyz/cymedical/tools/jun/ResponseTools.java
+++ src/xyz/cymedical/tools/jun/ResponseTools.java
@@ -11,7 +11,7 @@ public class ResponseTools {
 	 * 返回数据并且 跳回 跳转之前的页面
 	 */
 	public static String returnMsgAndBack(String msg) {
-		String strMsg="<script type='text/javascript'>alert('###');window.history.go(-1);</script>";
+		String strMsg="<script type='text/javascript'>alert('###');window.history.back();</script>";
 		return strMsg.replace("###", msg);
 	}
 	
