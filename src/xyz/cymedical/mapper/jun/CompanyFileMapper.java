7ef2a4ff207175bb88e10f8194deb0d726b8355b
Junisyoan <45847365+Junisyoan@users.noreply.github.com>
2019-01-22 21:45:18 +0800

    Update CompanyFileMapper.java

--- src/xyz/cymedical/mapper/jun/CompanyFileMapper.java
+++ src/xyz/cymedical/mapper/jun/CompanyFileMapper.java
@@ -1,5 +1,7 @@
 package xyz.cymedical.mapper.jun;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import xyz.cymedical.entity.jun.CompanyFile;
@@ -15,6 +17,13 @@ import xyz.cymedical.entity.jun.CompanyFile;
 @Repository
 public interface CompanyFileMapper {
 
+	/**
+	 * 查询文件列表
+	 * @param pageNo	页码
+	 * @return	文件列表
+	 */
+	public List<CompanyFile> queryFileList(int pageNo);
+	
 	/**
 	 * 文件信息插入数据库
 	 * @param file	文件信息
