84edf826f4f238a26674ac17550d2823fbdf5e99
437075478 <46699711+437075478@users.noreply.github.com>
2019-01-22 22:47:22 +0800

    Update PatientHandle.java

--- src/xyz/cymedical/handle/ctx/PatientHandle.java
+++ src/xyz/cymedical/handle/ctx/PatientHandle.java
@@ -29,11 +29,6 @@ public class PatientHandle {
 	@RequestMapping(value = "/findpatient.handle")
 	public ModelAndView findpatient(String name, String phone, String time, String code) {
 
-		System.out.println(name+"2222");
-		System.out.println(phone+"3333");
-		System.out.println(time+"4444");
-		System.out.println(code+"5555");
-		
 		patientlist = patientMapper.query(name, phone, time, code);
 
 		ModelAndView mav = new ModelAndView();
