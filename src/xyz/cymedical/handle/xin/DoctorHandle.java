364ec48144bd9c35397d6aab33008b2cbe56bfc5
451942369 <451942369@qq.com>
2019-01-22 14:43:13 +0800

    Update DoctorHandle.java

--- src/xyz/cymedical/handle/xin/DoctorHandle.java
+++ src/xyz/cymedical/handle/xin/DoctorHandle.java
@@ -2,6 +2,7 @@ package xyz.cymedical.handle.xin;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Resource;
 
@@ -10,9 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import xyz.cymedical.biz.xin.DoctorBiz;
-import xyz.cymedical.entity.xin.Patient_Project;
 import xyz.cymedical.entity.zsc.Project;
-
 
 @Controller
 @RequestMapping("/doctor")
@@ -21,24 +20,31 @@ public class DoctorHandle {
 	@Resource
 	private DoctorBiz doctorbiz; 			//医生的业务逻辑
 	
-	List<Patient_Project> plist = new ArrayList<Patient_Project>();
+	 List<Map<String,Object>> plist;
 	
-	//查询一维码对应病人的导检单
+
+	// 查询一维码对应病人的导检单
 	@RequestMapping(value = "/findProject.handle")
 	public ModelAndView findProject(String onecode) {
 
-		System.out.println("onecode="+onecode);
+		System.out.println("onecode=" + onecode);
 
 		System.out.println(doctorbiz.findMyProject(onecode));
 		
+<<<<<<< HEAD
+=======
+		plist=doctorbiz.findMyProject(onecode);
+
+
+>>>>>>> efd837ccd3ed6915c32478a050b39e45400d4765
 		ModelAndView mav = new ModelAndView();
 		mav.addObject("prolist", plist);
 		mav.setViewName("WEB-INF/doctor.xin/pro_receive");
 		return mav;
 
 	}
-	
-	//测试
+
+	// 测试
 	@RequestMapping(value = "/index.handle")
 	public ModelAndView find() {
 
@@ -47,25 +53,25 @@ public class DoctorHandle {
 
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("WEB-INF/doctor.xin/pro_receive");
-		
+
 		return mav;
 	}
-	
-	//根据选中项目跳转至对应小结
+
+	// 根据选中项目跳转至对应小结
 	@RequestMapping(value = "/Detail.handle")
-	public ModelAndView getDetail(String patientid,String projectid,String projectname) {
+	public ModelAndView getDetail(String patientid, String projectid, String projectname) {
 
 		System.out.println("getDetail....");
-		
-		System.out.println("patientid="+patientid);
-		System.out.println("projectid="+projectid);
-		System.out.println("projectname="+projectname);
+
+		System.out.println("patientid=" + patientid);
+		System.out.println("projectid=" + projectid);
+		System.out.println("projectname=" + projectname);
 
 		ModelAndView mav = new ModelAndView();
 		mav.addObject("projectname", projectname);
-		
+
 		mav.setViewName("WEB-INF/doctor.xin/brief");
 		return mav;
 	}
-	
+
 }
