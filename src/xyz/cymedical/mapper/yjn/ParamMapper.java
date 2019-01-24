a011b7d4f82d7bdf2efb959f8714558da446a09e
451942369 <451942369@qq.com>
2019-01-24 09:02:29 +0800

    Update ParamMapper.java

--- src/xyz/cymedical/mapper/yjn/ParamMapper.java
+++ src/xyz/cymedical/mapper/yjn/ParamMapper.java
@@ -2,8 +2,12 @@ package xyz.cymedical.mapper.yjn;
 
 import java.util.List;
 
-import xyz.cymedical.entity.yjn.Param;
+import org.springframework.stereotype.Repository;
 
+import xyz.cymedical.entity.yjn.Param;
+@Repository
 public interface ParamMapper {
 	public List<Param> findAllParam();
+	
+	public boolean delParam(String param_id);
 }
