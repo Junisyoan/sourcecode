1cdd56a34619b7343243cec1f9a7382707fc01b2
Junisyoan <45847365+Junisyoan@users.noreply.github.com>
2019-01-24 22:11:09 +0800

    Create ComboMapper.java

---
+++ src/xyz/cymedical/mapper/zsc/ComboMapper.java
@@ -0,0 +1,29 @@
+package xyz.cymedical.mapper.zsc;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Repository;
+
+import xyz.cymedical.entity.xin.Combo;
+
+@Repository
+public interface ComboMapper {
+
+	public int selectPrice(Map<String, Object> map);
+	
+	public int insertCombo(Combo combo);
+	
+	public int insertProCom(Map<String, Object> map);
+	
+	public int deleteCombo(int combo_id);
+	
+	public List<Combo> selectCombo(HashMap<String, Object> map);
+	
+	public Combo findCombo(String combo_id);
+	
+	public int updateCombo(Map<String, Object> map);
+	
+//	public int deleteDetPro(String project_id);
+}
