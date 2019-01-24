aeedf5c2b3d39b4bc3ea6776cb6f81e2d2561285
1720787484 <46743935+1720787484@users.noreply.github.com>
2019-01-24 10:59:02 +0800

    Update TbUserMapper.java

--- src/xyz/cymedical/mapper/jiang/TbUserMapper.java
+++ src/xyz/cymedical/mapper/jiang/TbUserMapper.java
@@ -1,6 +1,7 @@
 package xyz.cymedical.mapper.jiang;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Repository;
 
@@ -12,4 +13,18 @@ public interface TbUserMapper {
 	public List<Tb_user>  findUser(Tb_user user);
 
 	public List<Tb_user>  findAll();
+	
+	public List<Map<String,Object>> findAll2() ;
+	
+	public int  addUser(Tb_user user);
+	
+	public  int deleteUser(int user_id);
+	
+//	public  int upState(String state);
+	
+	public  int upState(int user_id,String state);
+	
+	public int upUser(Tb_user user);//修改人員信息
+	
+	
 }
