43a7e01da0f51d1bd561eeb1517dd9214b68fed6
1720787484 <46743935+1720787484@users.noreply.github.com>
2019-01-24 10:58:49 +0800

    Create UserManageHandle.java

---
+++ src/xyz/cymedical/handle/jiang/UserManageHandle.java
@@ -0,0 +1,203 @@
+package xyz.cymedical.handle.jiang;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import xyz.cymedical.biz.jiang.TbRoleDept;
+import xyz.cymedical.biz.jiang.TbUserBiz;
+import xyz.cymedical.entity.jiang.Tb_role_dept;
+import xyz.cymedical.entity.jiang.Tb_user;
+
+@Controller
+@RequestMapping("/usermanage")
+public class UserManageHandle {
+
+	 
+	private Tb_user user;
+ 
+	private List<Tb_user> userall;
+ 
+	private List<Tb_role_dept> roledept;
+	@Resource
+	private TbUserBiz tbUserBiz;
+	@Resource
+	private TbRoleDept tbRoleDept;
+	 
+	private List<Map<String,Object>> maplist;
+	
+	// 添加后台人员
+
+	@RequestMapping(value = "/adduser.handle")
+	public ModelAndView addUser(HttpServletRequest request, HttpServletResponse response, Tb_user adduser) {
+
+		ModelAndView mav = new ModelAndView();
+//		adduser.setRole_dept_id(1);
+//		System.out.println("doctor="+adduser.getDoctor());
+		System.out.println(adduser.getRole_dept_id());
+		System.out.println("douctor=" + adduser.getDoctor());
+		if (adduser.getDoctor().equals("内科医生")) {
+			adduser.setRole_dept_id(1);
+		} else if (adduser.getDoctor().equals("外科医生")) {
+			adduser.setRole_dept_id(2);
+		} else if (adduser.getDoctor().equals("管理员")) {
+			adduser.setRole_dept_id(3);
+		}
+
+		System.out.println("qqqqqqqqqqqq+Role_dept_id=" + adduser.getRole_dept_id());
+		int ret = tbUserBiz.addUser(adduser);
+
+		if (ret == 1) {
+			System.out.println("添加成功...");
+			mav.setViewName("usermanage");
+		}
+
+//		mav.setViewName("WEB-INF/view.jiang/index");
+		mav.setViewName("usermanage");
+
+		return mav;
+
+	}
+
+	// 显示
+	@RequestMapping(value = "/select.handle")
+	public ModelAndView findAll(HttpServletRequest request, HttpServletResponse response, Tb_user adduser) {
+
+		ModelAndView mav = new ModelAndView();
+
+		userall = tbUserBiz.findAll();
+		maplist=tbUserBiz.findAll2();
+		System.out.println("99999="+maplist.get(0).get("name"));
+		
+		System.out.println("aaaa....=" + userall);
+		if (null != maplist && maplist.size() > 0) {
+			
+			request.setAttribute("maplist", maplist);
+		} else {
+			System.out.println("沒有數據");
+		}
+
+		mav.setViewName("usermanage");
+		return mav;
+
+	}
+
+	// 删除 第一步
+	@RequestMapping(value = "/delect.handle", method = RequestMethod.POST)
+	public @ResponseBody String delect(HttpServletRequest request, HttpServletResponse response, String delectname) {
+		String data = null;
+		System.out.println("...=" + delectname);
+		int user_id = Integer.valueOf(delectname);
+		int ret = tbUserBiz.deleteUser(user_id);
+		if (ret == 1) {
+			data = "00";
+		}
+
+		return data;
+
+	}
+
+	// 部门查询
+	@RequestMapping(value = "/adddept.handle", method = RequestMethod.POST)
+//	, produces = "application/text;charset=utf-8"
+	public @ResponseBody String adddept(HttpServletRequest request, HttpServletResponse response, String dept) {
+		String addrole;
+		System.out.println("...=" + dept);
+		if (dept.equals("内科")) {
+			addrole = "01";
+		} else {
+			if (dept.equals("外科")) {
+				addrole = "02";
+			} else {
+				addrole = "03";
+
+			}
+		}
+		System.out.println("xxxxxxj" + addrole);
+		return addrole;
+
+	}
+
+	// 部門查询第二补
+	@RequestMapping(value = "/adddeptto.handle")
+	public ModelAndView adddeptto(HttpServletRequest request, HttpServletResponse response, String datato) {
+		String addrole;
+		System.out.println("...=" + datato);
+		ModelAndView mav = new ModelAndView();
+		int datadept = Integer.valueOf(datato);
+		if (datadept == 01) {
+			addrole = "内科";
+		} else {
+			if (datadept == 02) {
+				addrole = "外科";
+			} else {
+				addrole = "管理员";
+			}
+		}
+		request.setAttribute("addrole", addrole);
+
+		mav.setViewName("usermanage");
+		return mav;
+
+	}
+
+	// 禁用启用
+	@RequestMapping(value = "/updetestate.handle", method = RequestMethod.POST)
+	public @ResponseBody String updetestate(HttpServletRequest request, HttpServletResponse response, String userid,
+			String statet) {
+		String successs = null;
+		String state = null;
+		int user_id = Integer.valueOf(userid);
+		if (statet.equals("禁用")) {
+			state = "启用";
+		} else {
+			state = "禁用";
+		}
+		System.out.println("更改后的状态=" + state);
+
+		int ret = tbUserBiz.upState(user_id, state);
+		if (ret == 1) {
+			successs = "1";
+			System.out.println("修改成功");
+		} else {
+			successs = "0";
+			System.out.println("修改失败");
+		}
+
+		return successs;
+
+	}
+
+	// 修改人员信息一步
+	@RequestMapping(value = "/updete2.handle", method = RequestMethod.POST)
+	public ModelAndView updete2(HttpServletRequest request, HttpServletResponse response, String updetename) {
+		ModelAndView mav = new ModelAndView();
+
+		System.out.println("修改人员信息=" + updetename);
+		request.setAttribute("updetename", updetename);
+		mav.setViewName("WEB-INF/view.jiang/updeteuser");
+		return mav;
+
+	}
+
+	// 修改人员信息二部
+	@RequestMapping(value = "/updeteuser.handle", method = RequestMethod.POST)
+	public ModelAndView updeteuser(HttpServletRequest request, HttpServletResponse response, Tb_user upuser) {
+		ModelAndView mav = new ModelAndView();
+ 
+		tbUserBiz.upUser(upuser);
+
+		mav.setViewName("usermanage");
+		return mav;
+
+	}
+}
