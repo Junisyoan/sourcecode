68d2bcd95e7574701957bcb35cdfffe970da1285
1720787484 <46743935+1720787484@users.noreply.github.com>
2019-01-24 16:19:48 +0800

    Create RoleManageHandle.java

---
+++ src/xyz/cymedical/handle/jiang/RoleManageHandle.java
@@ -0,0 +1,66 @@
+package xyz.cymedical.handle.jiang;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import xyz.cymedical.biz.jiang.TbRoleBiz;
+import xyz.cymedical.entity.jiang.Tb_role;
+import xyz.cymedical.entity.jiang.Tb_user;
+
+@Controller
+@RequestMapping("/rolemanage")
+public class RoleManageHandle {
+	
+	
+	private  Tb_role role;
+	private List<Tb_role> roleall;
+	@Resource
+	private TbRoleBiz tbRoleBiz;
+	
+	
+	
+	
+	@RequestMapping(value = "/addrole.handle")
+	public ModelAndView addRole(HttpServletRequest request, HttpServletResponse response, String  name) {
+
+		ModelAndView mav = new ModelAndView();  
+		
+		
+		
+		
+		 System.out.println("++++++="+name);
+		int ret= tbRoleBiz.addRole(name);
+		 System.out.println("ret="+ret);
+		 if(ret==1) {
+			 System.out.println("添加成功"); 
+		 }else {
+			 System.out.println("添加失败");
+		 }
+		 mav.setViewName("WEB-INF/view.jiang/rolemanage");
+		  
+		return mav;
+
+	}
+	@RequestMapping(value = "/select.handle")
+	public ModelAndView selectRole(HttpServletRequest request, HttpServletResponse response, Tb_role addrole) {
+		
+		 
+		ModelAndView mav = new ModelAndView();  
+		roleall=tbRoleBiz.selectRole();
+		
+		request.setAttribute("roleall", roleall);
+		
+		mav.setViewName("WEB-INF/view.jiang/rolemanage");
+		
+		return mav;
+		
+	}
+
+}
