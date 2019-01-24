c88c809f7b5e4c255f3b1595857b5e4886ad4b7f
1720787484 <46743935+1720787484@users.noreply.github.com>
2019-01-24 16:19:31 +0800

    Create TbRoleBizImp.java

---
+++ src/xyz/cymedical/biz/imp/jiang/TbRoleBizImp.java
@@ -0,0 +1,38 @@
+package xyz.cymedical.biz.imp.jiang;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import xyz.cymedical.biz.jiang.TbRoleBiz;
+import xyz.cymedical.entity.jiang.Tb_role;
+import xyz.cymedical.mapper.jiang.TbRoleMapper;
+
+@Service("tbRoleBiz")
+public class TbRoleBizImp implements TbRoleBiz{
+	
+	 
+
+		@Resource
+		TbRoleMapper tbRoleMapper;
+		
+		
+	@Override
+	public List<Tb_role> selectRole() {
+		// TODO Auto-generated method stub
+		return tbRoleMapper.selectRole();
+	}
+
+
+	@Override
+	public int addRole(String name) {
+		// TODO Auto-generated method stub
+		return tbRoleMapper.addRole(name);
+	}
+
+
+	 
+
+}
