d42e2c3c04494df369b3fc6eab4afadf84625a8a
1720787484 <46743935+1720787484@users.noreply.github.com>
2019-01-25 08:57:57 +0800

    Update TbRoleBizImp.java

--- src/xyz/cymedical/biz/imp/jiang/TbRoleBizImp.java
+++ src/xyz/cymedical/biz/imp/jiang/TbRoleBizImp.java
@@ -33,6 +33,14 @@ public class TbRoleBizImp implements TbRoleBiz{
 	}
 
 
+	@Override
+	public Tb_role selectName(String name) {
+		// TODO Auto-generated method stub
+		return tbRoleMapper.selectName(name);
+//		return null;
+	}
+
+
 	 
 
 }
