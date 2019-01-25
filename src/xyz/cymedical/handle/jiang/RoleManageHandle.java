723645ea9b3e6b442a34395420777d1ace26d021
1720787484 <46743935+1720787484@users.noreply.github.com>
2019-01-25 08:58:04 +0800

    Update RoleManageHandle.java

--- src/xyz/cymedical/handle/jiang/RoleManageHandle.java
+++ src/xyz/cymedical/handle/jiang/RoleManageHandle.java
@@ -1,5 +1,6 @@
 package xyz.cymedical.handle.jiang;
 
+import java.io.IOException;
 import java.util.List;
 
 import javax.annotation.Resource;
@@ -32,17 +33,30 @@ public class RoleManageHandle {
 
 		ModelAndView mav = new ModelAndView();  
 		
-		
-		
-		
+			role=tbRoleBiz.selectName(name);
+			if(role!=null) {
+			System.out.println("账号已存在");
+			try {
+				response.getWriter().print("<script type='text/javascript'>alert('账号已存在');window.history.back();</script>");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			}else {
+				
+			
 		 System.out.println("++++++="+name);
 		int ret= tbRoleBiz.addRole(name);
 		 System.out.println("ret="+ret);
 		 if(ret==1) {
 			 System.out.println("添加成功"); 
+			 
 		 }else {
 			 System.out.println("添加失败");
 		 }
+			}
+			
 		 mav.setViewName("WEB-INF/view.jiang/rolemanage");
 		  
 		return mav;
