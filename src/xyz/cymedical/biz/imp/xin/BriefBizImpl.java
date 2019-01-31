8615b6f5746b8738576d3295a152ab1ecd9fc456
524074949 <46743055+524074949@users.noreply.github.com>
2019-01-31 20:18:01 +0800

    Create BriefBizImpl.java

---
+++ src/xyz/cymedical/biz/imp/xin/BriefBizImpl.java
@@ -0,0 +1,44 @@
+package xyz.cymedical.biz.imp.xin;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import xyz.cymedical.biz.xin.BriefBiz;
+import xyz.cymedical.biz.xin.DoctorBiz;
+import xyz.cymedical.mapper.xin.BriefMapper;
+import xyz.cymedical.mapper.xin.DoctorMapper;
+
+/**
+* 2019年1月20日
+* @author xin
+* @version 1.0
+*/
+
+@Service("briefbiz")
+public class BriefBizImpl  implements BriefBiz {
+
+	@Resource
+	private BriefMapper briefMapper;
+
+	@Override
+	public List<Map<String, Object>> Photo(String code) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean Check(String result, String tips, String id) {
+		// TODO Auto-generated method stub
+		return briefMapper.Check(result, tips, id);
+	}
+
+	@Override
+	public boolean Normal(String result, String id) {
+		// TODO Auto-generated method stub
+		return briefMapper.Normal(result, id);
+	}
+}
