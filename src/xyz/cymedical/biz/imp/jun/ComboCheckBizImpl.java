84e3667f2835b22a42bf000e1f299905c53d83d3
1720787484 <46743935+1720787484@users.noreply.github.com>
2019-02-01 20:00:18 +0800

    Create ComboCheckBizImpl.java

---
+++ src/xyz/cymedical/biz/imp/jun/ComboCheckBizImpl.java
@@ -0,0 +1,33 @@
+package xyz.cymedical.biz.imp.jun;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import xyz.cymedical.biz.jun.ComboCheckBiz;
+import xyz.cymedical.mapper.jun.ComboCheckMapper;
+
+/**
+*	@author Junisyoan;
+*	日期：2019年1月27日
+*	时间：下午10:18:12
+*	类说明：
+*/
+
+@Service("comboCheckBiz")
+public class ComboCheckBizImpl extends BaseImpl implements ComboCheckBiz {
+
+	@Resource
+	private ComboCheckMapper comboCheckMapper;
+	
+	@Override
+	public boolean queryCombo(String comboName) {
+		if (comboCheckMapper.queryComboByName(comboName)!=null) {
+			isExsit=true;
+		} else {
+			isExsit=false;
+		}
+		return isExsit;
+	}
+
+}
