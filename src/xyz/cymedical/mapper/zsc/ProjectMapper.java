2f4a9e9cacce118c3f757ff46f70a1824aea2ca6
1720787484 <46743935+1720787484@users.noreply.github.com>
2019-02-01 20:03:25 +0800

    Create ProjectMapper.java

---
+++ src/xyz/cymedical/mapper/zsc/ProjectMapper.java
@@ -0,0 +1,29 @@
+package xyz.cymedical.mapper.zsc;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Repository;
+
+import xyz.cymedical.entity.zsc.Project;
+
+@Repository
+public interface ProjectMapper {
+
+	public int selectParamtId(String param_name);
+	
+	public int insertProject(Project project);
+	
+	public int insertDetPro(Map<String, Object> map);
+	
+	public int deleteProject(int project_id);
+	
+	public List<Project> selectProject(HashMap<String, Object> map);
+	
+	public Project findProject(String project_id);
+	
+	public int updateProject(Map<String, Object> map);
+
+	public int checkName(Map<String, Object> map);
+}
