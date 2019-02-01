fe4784d4d416a9b57b2fc7c01cd020e06d9369aa
1720787484 <46743935+1720787484@users.noreply.github.com>
2019-02-01 20:02:36 +0800

    Create GroupMapper.java

---
+++ src/xyz/cymedical/mapper/jun/GroupMapper.java
@@ -0,0 +1,24 @@
+package xyz.cymedical.mapper.jun;
+
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+/**
+*	@author Junisyoan;
+*	日期：2019年1月29日
+*	时间：下午11:47:21
+*	类说明：映射类
+*/
+
+@Repository
+public interface GroupMapper {
+	
+	/**
+	 * 生成团检表
+	 * @param cid	公司id
+	 * @param pnum	人数
+	 * @param time	生成时间
+	 * @return	是否成功
+	 */
+	public boolean insert(@Param("cid")int cid,@Param("pnum")int pnum,@Param("time")String time);
+}
