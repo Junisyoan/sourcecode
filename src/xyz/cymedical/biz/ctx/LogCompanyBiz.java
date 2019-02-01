04c0a97e1c8e8c38ce7efd0354555a6786785325
1720787484 <46743935+1720787484@users.noreply.github.com>
2019-02-01 19:59:52 +0800

    Update LogCompanyBiz.java

--- src/xyz/cymedical/biz/ctx/LogCompanyBiz.java
+++ src/xyz/cymedical/biz/ctx/LogCompanyBiz.java
@@ -13,6 +13,16 @@ import xyz.cymedical.entity.ctx.LogCompany;
 
 public interface LogCompanyBiz {
 
+	/**
+	 * @author Junisyoan
+	 * @param cid	公司id
+	 * @param op	操作
+	 * @param deposit	金额
+	 * @param t	操作时间
+	 * @return	是否插入成功
+	 */
+	public boolean insertLog(int cid,String op,String deposit,String t);
+	
 	public List<LogCompany> queryByName(String name);
 
 }
