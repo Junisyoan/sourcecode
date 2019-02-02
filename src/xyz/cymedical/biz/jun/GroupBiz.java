7277456682430a1319009c3a22554d7814bcf4ea
Junisyoan <45847365+Junisyoan@users.noreply.github.com>
2019-02-02 18:16:51 +0800

    Update GroupBiz.java

--- src/xyz/cymedical/biz/jun/GroupBiz.java
+++ src/xyz/cymedical/biz/jun/GroupBiz.java
@@ -1,5 +1,6 @@
 package xyz.cymedical.biz.jun;
 
+import xyz.cymedical.entity.jun.Group;
 
 /**
 *	@author Junisyoan;
@@ -8,13 +9,29 @@ package xyz.cymedical.biz.jun;
 *	类说明：导检单操作
 */
 public interface GroupBiz {
+	
+	
+	/**
+	 * 更新软件表信息
+	 * @param fid	文件表id
+	 * @param pnum	人数
+	 * @param price	价格
+	 * @return	是否修改成功
+	 */
+	public boolean updateGroupInfo(int fid,int pnum, float price);
+	
+	/**
+	 * 查询团检表
+	 * @param fid	文件Id
+	 * @return	团检表
+	 */
+	public Group queryGroupByFileId(int fid);
 
 	/**
-	 * 生成团检表
+	 * 创建团检表
 	 * @param cid	公司id
-	 * @param pnum	人数
-	 * @param time	生成时间
-	 * @return	是否成功
+	 * @param fid	文件id
+	 * @return	是否创建成功
 	 */
-	public boolean insert(int cid,int pnum,String time);
+	public boolean createGroup(int cid, int fid);
 }
