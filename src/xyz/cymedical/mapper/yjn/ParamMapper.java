6faedc64a184bd9ed5c2b0577de218b475f9464d
437075478 <46699711+437075478@users.noreply.github.com>
2019-02-09 00:14:34 +0800

    Update ParamMapper.java

--- src/xyz/cymedical/mapper/yjn/ParamMapper.java
+++ src/xyz/cymedical/mapper/yjn/ParamMapper.java
@@ -2,12 +2,19 @@ package xyz.cymedical.mapper.yjn;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
+import xyz.cymedical.entity.yjn.Parameter;
 
-import xyz.cymedical.entity.yjn.Param;
 @Repository
 public interface ParamMapper {
-	public List<Param> findAllParam();
-	
+
+	public List<Parameter> findAllParam();
+
 	public boolean delParam(String param_id);
+
+	public List<Parameter> searchParam(@Param("param_id") String param_id, @Param("pid") String pid,
+			@Param("name") String name);
+
+	public boolean insertParam(@Param("pid") String pid, @Param("name") String name);
 }
