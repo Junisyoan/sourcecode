7023fc6ea4ba3c722749c26deefe7dd8f492dc2d
437075478 <46699711+437075478@users.noreply.github.com>
2019-02-09 00:12:25 +0800

    Update ParamBizImpl.java

--- src/xyz/cymedical/biz/imp/yjn/ParamBizImpl.java
+++ src/xyz/cymedical/biz/imp/yjn/ParamBizImpl.java
@@ -7,23 +7,35 @@ import javax.annotation.Resource;
 import org.springframework.stereotype.Service;
 
 import xyz.cymedical.biz.yjn.ParamBiz;
-import xyz.cymedical.entity.yjn.Param;
+import xyz.cymedical.entity.yjn.Parameter;
 import xyz.cymedical.mapper.yjn.ParamMapper;
 
 @Service("paramBiz")
-public class ParamBizImpl implements ParamBiz{
+public class ParamBizImpl implements ParamBiz {
 
 	@Resource
 	private ParamMapper parammapper;
-	
+
 	@Override
-	public List<Param> findAllParam() {
+	public List<Parameter> findAllParam() {
 		return parammapper.findAllParam();
 	}
 
 	@Override
 	public boolean delParam(String param_id) {
 		return parammapper.delParam(param_id);
+	}
+
+	@Override
+	public List<Parameter> searchParam(String param_id, String pid, String name) {
+		// TODO Auto-generated method stub
+		return parammapper.searchParam(param_id, pid, name);
+	}
+
+	@Override
+	public boolean insertParam(String pid, String name) {
+		// TODO Auto-generated method stub
+		return parammapper.insertParam(pid, name);
 	}
 
 }
