7d31224c415487f1aa99828b4b7bfa0106b90cf6
1720787484 <46743935+1720787484@users.noreply.github.com>
2019-02-09 16:01:47 +0800

    Create TbPowerBizImp.java

---
+++ src/xyz/cymedical/biz/imp/jiang/TbPowerBizImp.java
@@ -0,0 +1,36 @@
+package xyz.cymedical.biz.imp.jiang;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import xyz.cymedical.biz.jiang.TbPowerBiz;
+import xyz.cymedical.entity.jiang.Tb_power;
+import xyz.cymedical.mapper.jiang.TbPowerMapper;
+
+@Service("tbPowerBiz")
+public class TbPowerBizImp implements TbPowerBiz{
+	
+	@Resource
+	TbPowerMapper tbPowerMapper;
+	
+	@Override
+	public List<Tb_power> selectPower() {
+		return tbPowerMapper.selectPower();
+	}
+
+	@Override
+	public int deletePower(int power_id) {
+		// TODO Auto-generated method stub
+		return tbPowerMapper.deletePower(power_id);
+	}
+
+	@Override
+	public int addPower(Tb_power tb_power) {
+		// TODO Auto-generated method stub
+		return tbPowerMapper.addPower(tb_power);
+	}
+
+}
