20a935fa1d9efb081480b9e6f8d211ae336b4bed
1720787484 <46743935+1720787484@users.noreply.github.com>
2019-02-10 21:24:55 +0800

    Update DeptManageHandle.java

--- src/xyz/cymedical/handle/jiang/DeptManageHandle.java
+++ src/xyz/cymedical/handle/jiang/DeptManageHandle.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import xyz.cymedical.biz.jiang.TbDeptBiz;
@@ -37,4 +39,73 @@ public class DeptManageHandle {
  
 		 
 	}
+	//**添加科室//*	
+	@RequestMapping(value = "/adddept.handle")
+	public ModelAndView addPower(HttpServletRequest request, HttpServletResponse response, Tb_dept tb_dept) {
+		ModelAndView mav = new ModelAndView();  
+		 
+//		int ret=TbPowerBiz.addPower(tb_power);
+		System.out.println(tb_dept.getDept_id());
+		System.out.println(tb_dept.getName());
+		int ret=tbDeptBiz.addDept(tb_dept);
+		
+		
+		if(ret==1) {
+			
+			mav.setViewName("WEB-INF/view.jiang/deptmanage");
+		}
+		  
+		return mav;
+
+	}
+	
+	
+	//修改第一步
+	@RequestMapping(value = "/updete1.handle", method = RequestMethod.POST)
+	public ModelAndView updete2(HttpServletRequest request, HttpServletResponse response, String updetename) {
+		ModelAndView mav = new ModelAndView();
+
+		System.out.println("修改人员信息=" + updetename);
+		request.setAttribute("updetename", updetename);
+		mav.setViewName("WEB-INF/view.jiang/updetedept");
+		return mav;
+
+	}
+	
+	//修改第二步
+	 
+			@RequestMapping(value = "/updetepower2.handle", method = RequestMethod.POST)
+			public ModelAndView updeteuser(HttpServletRequest request, HttpServletResponse response, Tb_dept tb_dept) {
+				ModelAndView mav = new ModelAndView();
+
+
+				int  ret=tbDeptBiz.upDept(tb_dept);
+				
+				 	if(ret==1) {
+				 		
+				 		mav.setViewName("WEB-INF/view.jiang/deptmanage");
+				 	}
+
+				return mav;
+
+			}
+			
+			//删除
+			
+
+			@RequestMapping(value = "/delect.handle",method = RequestMethod.POST)
+			public @ResponseBody String deletePower(HttpServletRequest request, HttpServletResponse response, String delectname) {
+ 				
+				System.out.println("delectname="+delectname);
+				String data = null;
+				int dept_id=Integer.valueOf(delectname);
+				int ret=tbDeptBiz.delectDept(dept_id);
+				
+				if(ret==1) { 
+					data = "00";
+				}
+				  
+				return data;
+	 	
+			}
 }
