f50f99789cfbad58bd06cee4245544de974f3acc
1720787484 <46743935+1720787484@users.noreply.github.com>
2019-02-12 20:58:19 +0800

    Update RoleManageHandle.java

--- src/xyz/cymedical/handle/jiang/RoleManageHandle.java
+++ src/xyz/cymedical/handle/jiang/RoleManageHandle.java
@@ -1,7 +1,9 @@
 package xyz.cymedical.handle.jiang;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -13,7 +15,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import net.sf.json.JSONArray;
 import xyz.cymedical.biz.jiang.TbRoleBiz;
+import xyz.cymedical.entity.jiang.Tb_dept;
 import xyz.cymedical.entity.jiang.Tb_role;
 import xyz.cymedical.entity.jiang.Tb_user;
 
@@ -130,5 +134,17 @@ public class RoleManageHandle {
 					}
 					 return mav;
 
+				}
+				
+				// 查
+				@RequestMapping(value = "/selectCompany.handle", method = RequestMethod.POST, produces = "application/json;charset=utf-8")
+				public @ResponseBody String selectCompany(Tb_role tb_role) { 
+					Map<String, Object> map = new HashMap<String, Object>(); 
+					map.put("tb_role", tb_role);
+					 
+					List<Map<String,Object>> companys = tbRoleBiz.selectCompany(map);
+//					List<Tb_user> companys = companyBizsc.selectCompany(map);
+					String str = JSONArray.fromObject(companys).toString();  
+					return str;
 				}
 }
