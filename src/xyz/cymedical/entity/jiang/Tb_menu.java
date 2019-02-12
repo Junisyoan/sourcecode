6674a74d3f274f4d3535da1f1726fdfdf7eb199f
1720787484 <46743935+1720787484@users.noreply.github.com>
2019-02-12 20:58:09 +0800

    Update Tb_menu.java

--- src/xyz/cymedical/entity/jiang/Tb_menu.java
+++ src/xyz/cymedical/entity/jiang/Tb_menu.java
@@ -7,16 +7,30 @@ public class Tb_menu {
 	private int menu_id;//菜单表id
 	private String name;//菜单名字
 	private String link;//路径
+	private int superior;
 	
 	public Tb_menu() {
 		super();
 	}
-
-	public Tb_menu(int menu_id, String name, String link) {
+ 
+	public Tb_menu(int menu_id, String name, String link, int superior) {
 		super();
 		this.menu_id = menu_id;
 		this.name = name;
 		this.link = link;
+		this.superior = superior;
+	}
+
+
+ 
+
+
+	public int getSuperior() {
+		return superior;
+	}
+
+	public void setSuperior(int superior) {
+		this.superior = superior;
 	}
 
 	public int getMenu_id() {
