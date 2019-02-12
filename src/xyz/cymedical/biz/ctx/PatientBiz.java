f5508d59dbcaa2fcb5bb1fb41e175c50980d3939
Junisyoan <45847365+Junisyoan@users.noreply.github.com>
2019-02-12 16:44:41 +0800

    Update PatientBiz.java

--- src/xyz/cymedical/biz/ctx/PatientBiz.java
+++ src/xyz/cymedical/biz/ctx/PatientBiz.java
@@ -16,7 +16,10 @@ public interface PatientBiz {
 	 */
 	public List<Patient> insertByBatch(List<Patient> listRecord);
 
-	public List<Patient> query(@Param("name") String name, @Param("phone") String phone, @Param("time") String time,
+	public List<Patient> query(
+			@Param("name") String name, 
+			@Param("phone") String phone, 
+			@Param("time") String time,
 			@Param("code") String code);
 
 	public List<Patient> queryproject(@Param("name") String name, @Param("time") String time);
