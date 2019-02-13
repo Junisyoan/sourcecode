6e70b823fffe922d0cf58831a533b3d41fbf1965
Junisyoan <45847365+Junisyoan@users.noreply.github.com>
2019-02-13 20:54:37 +0800

    Update LoginHandle.java

--- src/xyz/cymedical/handle/yjn/LoginHandle.java
+++ src/xyz/cymedical/handle/yjn/LoginHandle.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import xyz.cymedical.biz.jiang.TbUserBiz;
+import xyz.cymedical.biz.xin.DoctorBiz;
+import xyz.cymedical.entity.jiang.Tb_menu;
 import xyz.cymedical.entity.jiang.Tb_user;
 
 @Controller // 此注释的含义是将该类设置成为浏览器提交的上来的类
@@ -21,41 +24,66 @@ public class LoginHandle {
 
 	@Resource
 	public TbUserBiz tbUserBiz;
+	
+	@Resource
+	private DoctorBiz doctorbiz;
+	
+	private Tb_user userr;
 
 	private List<Tb_user> userlist = new ArrayList<>();
+	private List<Tb_user> userlist2 = new ArrayList<>();
 
 	@RequestMapping(value = "/login.handle", method = RequestMethod.POST, produces = "application/json;charset=utf-8")
 	public @ResponseBody String login(HttpServletRequest req, String checkCode, Tb_user user) {
+		HttpSession session = req.getSession();
 
 		String result = "";
 
-		String mapcode = (String) req.getSession().getAttribute("CODE");
-
-		System.out.println("mapcode=" + mapcode);
-
-		System.out.println("checkCode=" + checkCode);
-
 		userlist = tbUserBiz.findUser(user);
+		userlist2 = tbUserBiz.findUserRole(user);
 
-		if (userlist.size() > 0 && userlist.get(0).getRole_dept_id() == 3) {
+		System.out.println("User"+userlist.size());
+		System.out.println("UserRole"+userlist2.size());
+		
+		if (userlist.size() > 0 && userlist2.size() > 0) {
 			result = "管理员";
-		} else if (user.getAccount().equals("admin") && user.getPwd().equals("1234")) {
-			result = "医生";
-		} else if (user.getAccount().equals("admin") && user.getPwd().equals("123456")) {
-			result = "后台";
 		} else {
 			result = "账号密码错误";
 		}
+
+		session.setAttribute("user", userlist.get(0));
+
 		return result;
 
 	}
 
 	@RequestMapping(value = "/index.handle")
-	public ModelAndView find() {
+	public ModelAndView find(HttpServletRequest req) {
 
 		ModelAndView mav = new ModelAndView();
+		userr=(Tb_user) req.getSession().getAttribute("user");
+		
+		if(userr!=null) {
+			//用户对应菜单列表
+			List<Tb_menu> mlist = doctorbiz.getMyMenu(userr.getUser_id(),userr.getRole_dept_id());
+			mav.addObject("mlist", mlist);
+			mav.addObject("USER", userr);
+		}
+		
+		
+		
 		mav.setViewName("WEB-INF/view.jiang/index");
 		return mav;
+
+	}
+
+	@RequestMapping(value = "/modify.handle")
+	public ModelAndView modify(String oldpwd, String newpwd) {
+		
+		System.out.println(oldpwd);
+		System.out.println(newpwd);
+
+		return null;
 
 	}
 
