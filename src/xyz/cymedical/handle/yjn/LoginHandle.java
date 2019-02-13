616832720dc866be3c489c4e295e9ac6d99dce78
437075478 <46699711+437075478@users.noreply.github.com>
2019-02-13 16:56:02 +0800

    Update LoginHandle.java

--- src/xyz/cymedical/handle/yjn/LoginHandle.java
+++ src/xyz/cymedical/handle/yjn/LoginHandle.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import xyz.cymedical.biz.jiang.TbUserBiz;
+import xyz.cymedical.biz.xin.DoctorBiz;
+import xyz.cymedical.entity.jiang.Tb_menu;
 import xyz.cymedical.entity.jiang.Tb_user;
 
 @Controller // 此注释的含义是将该类设置成为浏览器提交的上来的类
@@ -22,6 +24,11 @@ public class LoginHandle {
 
 	@Resource
 	public TbUserBiz tbUserBiz;
+	
+	@Resource
+	private DoctorBiz doctorbiz;
+	
+	private Tb_user userr;
 
 	private List<Tb_user> userlist = new ArrayList<>();
 	private List<Tb_user> userlist2 = new ArrayList<>();
@@ -41,6 +48,9 @@ public class LoginHandle {
 		userlist = tbUserBiz.findUser(user);
 		userlist2 = tbUserBiz.findUserRole(user);
 
+		System.out.println("User"+userlist.size());
+		System.out.println("UserRole"+userlist2.size());
+		
 		if (userlist.size() > 0 && userlist2.size() > 0) {
 			result = "管理员";
 		} else {
@@ -54,9 +64,20 @@ public class LoginHandle {
 	}
 
 	@RequestMapping(value = "/index.handle")
-	public ModelAndView find() {
+	public ModelAndView find(HttpServletRequest req) {
 
 		ModelAndView mav = new ModelAndView();
+		userr=(Tb_user) req.getSession().getAttribute("user");
+		
+		if(userr!=null) {
+			//用户对应菜单列表
+			List<Tb_menu> mlist = doctorbiz.getMyMenu(userr.getUser_id(),userr.getRole_dept_id());
+			mav.addObject("mlist", mlist);
+			mav.addObject("USER", userr);
+		}
+		
+		
+		
 		mav.setViewName("WEB-INF/view.jiang/index");
 		return mav;
 
