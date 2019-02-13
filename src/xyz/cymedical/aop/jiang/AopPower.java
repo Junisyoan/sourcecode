81d88eda2bd3239758c58d6d0c538f5426e062e9
Junisyoan <45847365+Junisyoan@users.noreply.github.com>
2019-02-13 20:56:28 +0800

    Create AopPower.java

---
+++ src/xyz/cymedical/aop/jiang/AopPower.java
@@ -0,0 +1,13 @@
+package xyz.cymedical.aop.jiang;
+
+import javax.annotation.Resource;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect    // 切面注解
+@Component //通用注解
+public class AopPower {
+	
+	
+}
