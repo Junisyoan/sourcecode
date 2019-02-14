4c0d6c429c129579587248a3c353864786bd7de5
524074949 <46743055+524074949@users.noreply.github.com>
2019-02-14 11:57:55 +0800

    Update ComboBiz.java

--- src/xyz/cymedical/biz/zsc/ComboBiz.java
+++ src/xyz/cymedical/biz/zsc/ComboBiz.java
@@ -7,54 +7,48 @@ import java.util.Map;
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import xyz.cymedical.aop.zsc.Log;
 import xyz.cymedical.entity.xin.Combo;
 import xyz.cymedical.mapper.zsc.ComboMapper;
 
+@Transactional(rollbackFor = Exception.class)
 @Service
 public class ComboBiz {
 
 	@Resource
 	ComboMapper comboMapper;
-	
-	public int insertCombo(Combo combo,int[] idArray) {
-		
+
+	@Log(action = "添加套餐")
+	public int insertCombo(Combo combo, int[] idArray) {
+
 		Map<String, Object> map = new HashMap<String, Object>();
 		map.put("idArray", idArray);
-		
+
 		if (combo.getPrice() == 0) {
 			int price = comboMapper.selectPrice(map);
 			combo.setPrice(price);
 		}
-		
+
 		comboMapper.insertCombo(combo);
 		map.put("combo", combo);
-		
+
 		int rt = comboMapper.insertProCom(map);
 		return rt;
 	}
-	
-	public List<Combo> findCombos(){
-		return comboMapper.selectCombo(null);
-	}
-	
+
+	@Log(action = "删除套餐")
 	public int deleteCombo(int combo_id) {
 		return comboMapper.deleteCombo(combo_id);
 	};
-	
-	public List<Combo> selectCombo(HashMap<String, Object> map){
-		return comboMapper.selectCombo(map);
-	}
-	
-	public Combo findCombo(String combo_id){
-		return comboMapper.findCombo(combo_id);
-	}
-	
+
+	@Log(action = "修改套餐")
 	public String updateCombo(int[] idArray, Combo combo) {
 		Map<String, Object> map = new HashMap<String, Object>();
 		map.put("idArray", idArray);
 		map.put("combo", combo);
-		
+
 		int rt = comboMapper.updateCombo(map);
 		if (rt > 0) {
 			return "修改成功";
@@ -62,7 +56,18 @@ public class ComboBiz {
 			return "修改失败";
 		}
 	}
-	
+
+	public List<Combo> findCombos() {
+		return comboMapper.selectCombo(null);
+	}
+
+	public List<Combo> selectCombo(HashMap<String, Object> map) {
+		return comboMapper.selectCombo(map);
+	}
+
+	public Combo findCombo(String combo_id) {
+		return comboMapper.findCombo(combo_id);
+	}
 
 	public String checkName(Map<String, Object> map) {
 		int rt = comboMapper.checkName(map);
